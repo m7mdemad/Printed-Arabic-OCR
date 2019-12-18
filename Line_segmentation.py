2320839fc212a5f8da0b1971792bbb16d71d91cd
m7mdemad <omdaemad28@yahoo.com>
2019-12-18 20:52:17 +0200

    Add files via upload

--- Line_segmentation.py
+++ Line_segmentation.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 from newChar import get_characters
+from WordSegmentor import WordSegmentor
+from LineSegmentor import LineSegmentor
 
 def horizintal_projection(im):
     #im = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -53,6 +55,26 @@ def get_words(lines):
     return Lines_in_words
 
 
+
+def skew_correction(image):
+    gray = cv2.bitwise_not(image)
+
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    coords = np.column_stack(np.where(thresh > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+
+    if angle < -45:
+        angle = -(90 + angle)
+    else:
+        angle = -angle
+
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+
+    return rotated
+
 def segment_paragragh(lines, words):
 
     # img = cv2.imread('image.png',0)
@@ -82,10 +104,10 @@ def segment_paragragh(lines, words):
  
 
 #def tempFunction():
-#	img = cv2.imread('img.png', 0)
-#	img = skew_correction(img)
-#	lines, lines_dil = LineSegmentor(img).segment_lines()
-#   words, length = WordSegmentor(lines, lines_dil).segment_words() 
-#	return segment_paragragh(lines, words)
+#    img = cv2.imread('image.png', 0)
+#    img = skew_correction(img)
+#    lines, lines_dil = LineSegmentor(img).segment_lines()
+#    words, length = WordSegmentor(lines, lines_dil).segment_words() 
+#    return segment_paragragh(lines, words)
 
 #tempFunction()
